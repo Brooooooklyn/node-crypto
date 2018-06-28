dde1d4f8ed5fde54296ee8b623f61cb9f7bb8620
LongYinan <lynweklm@gmail.com>
2018-06-28 15:39:05 +0800

    fix: use request to download

--- download.js
+++ download.js
@@ -1,27 +1,31 @@
 const fs = require('fs')
 const version = require('./package.json').version
-const https = require('https')
+const request = require('request')
 const semver = require('semver')
 const child_process = require('child_process')
 
 const MAJOR_VERSION = semver.parse(process.version).major
 const HOST = 'https://storage.googleapis.com/storage.lynvv.xyz'
+const FILE_NAME = `crypto-node-${ process.platform }-${ MAJOR_VERSION }-${ version }`
 
-https.get(`${HOST}/crypto-node-${ process.platform }-${ MAJOR_VERSION }-${ version }`).pipe(
-  fs.createWriteStream('index.node')
-)
+request.get(`${ HOST }/${ FILE_NAME }`)
+  .pipe(
+    fs.createWriteStream('index.node')
+  )
   .on('error', (e) => {
     console.error(e)
     fs.unlinkSync('index.node')
   })
   .on('close', () => {
     const size = fs.statSync('index.node').size
-    if (!size) {
+    if (size < 10000) {
       console.error('Download prebuilt binary fail, fallback to build')
       fs.unlinkSync('index.node')
       child_process.execSync('npm run build', {
         stdio: [0, 1, 2]
       })
       fs.copyFileSync('./native/index.node', 'index.node')
+    } else {
+      console.log('download prebuilt file success ', FILE_NAME)
     }
   })
